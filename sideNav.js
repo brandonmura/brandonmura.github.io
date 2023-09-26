a20ee164073c52ead3b8e24d1bf65f4eabd44fa8
brandonmura <mura0005@hz.nl>
2023-09-26 14:35:42 +0200

    blog.html

---
+++ sideNav.js
@@ -0,0 +1,8 @@
+function openSideNav() {
+  document.getElementById("mySideNav").style.width = "20%";
+  document.getElementById("openSideNavBtn").style.opacity = "0";
+}
+function closeSideNav() {
+  document.getElementById("mySideNav").style.width = "0";
+  document.getElementById("openSideNavBtn").style.opacity = "1";
+}
